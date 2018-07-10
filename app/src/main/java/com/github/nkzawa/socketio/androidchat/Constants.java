c00d9bdb815ff4cb9718e176144ad15d738b1510
HanYoungJu <1025hyj@naver.com>
2018-07-11 00:54:42 +0900

    Initial commit

---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -0,0 +1,6 @@
+package com.github.nkzawa.socketio.androidchat;
+
+public class Constants {
+    public static final String SERVER_URL = "http://52.231.71.194:8080/";
+}
+//https://socket-io-chat.now.sh/
---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/UmmkApplication.java
@@ -0,0 +1,23 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.app.Application;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class UmmkApplication extends Application {
+
+    private static Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket(Constants.SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Socket getSocket() {
+        return mSocket;
+    }
+}
